d275d3794820766610f6ce0c9c0153fed705babd
fitz-dev <tom.waters@myob.com>
2020-10-19 16:36:15 +1100

    made the logic to randomise the number in the cell

--- app.js
+++ app.js
@@ -1,13 +1,41 @@
-// function checkOffCell() {
-//     window.onclick = e => {
-//         if (e.target.classList.contains("board-cell")) {
-//             e.target.classList.toggle("picked")
-//         }
-//     }
-// }
-
 document.addEventListener('click', function (event) {
     if (event.target.classList.contains("board-cell")) {
         event.target.classList.toggle("picked")
     }
 });
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function CreateListOfRandomNumbers() {
+    let numbers = [];
+    const sizeOfGrid = 25;
+    while (numbers.length < sizeOfGrid) {
+        let randomNumber = getRandomInt(1, sizeOfGrid + 1);
+        if (!numbers.includes(randomNumber)) {
+            numbers.push(randomNumber)
+        }
+    }
+    return numbers;
+}
+
+function AssignCellContent() {
+    let numbers = CreateListOfRandomNumbers();
+    let cells = document.querySelectorAll(".board-cell");
+    var counter = 0;
+    var centrePosition = 12;
+    cells.forEach(element => {
+        if (counter == centrePosition) {
+            element.innerHTML = "&#10033;"
+        }
+        else {
+            element.innerHTML = numbers[counter];
+        }
+        counter++;
+    });
+}
+
+AssignCellContent();
